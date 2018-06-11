df5f85da1dcd0968a36d37ddc153ea1e575120f9
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-06-10 19:40:08 -0500

    delete not working. HELP

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,13 +1,13 @@
 const singleWeatherDom = (singleWeather) => {
   let singleString = '';
   singleString += `<div class="jumbotron weather">`;
-  singleString += `<h1>City Name: ${singleWeather.name}</h1>`;
-  singleString += `<img src="https://openweathermap.org/img/w/${singleWeather.weather[0].icon}.png" alt="image of weather">`;
-  singleString += `<h2>Temperature: ${singleWeather.main.temp}</h2>`;
-  singleString += `<h2>Description: ${singleWeather.weather[0].description}</h2>`;
-  singleString += `<h3> Air Pressure: ${singleWeather.main.pressure}</h3>`;
-  singleString += `<h3>Wind Speed: ${singleWeather.wind.speed}</h3>`;
-  singleString += `<p><a id="fivedayBtn" class="btn btn-primary btn-lg" role="button">5 Day Forecast</a></p>`;
+  singleString +=   `<h1>City Name: ${singleWeather.name}</h1>`;
+  singleString +=   `<img src="https://openweathermap.org/img/w/${singleWeather.weather[0].icon}.png" alt="image of weather">`;
+  singleString +=   `<h2>Temperature: ${singleWeather.main.temp}</h2>`;
+  singleString +=   `<h2>Description: ${singleWeather.weather[0].description}</h2>`;
+  singleString +=   `<h3> Air Pressure: ${singleWeather.main.pressure}</h3>`;
+  singleString +=   `<h3>Wind Speed: ${singleWeather.wind.speed}</h3>`;
+  singleString +=   `<p><a id="fivedayBtn" class="btn btn-primary btn-lg" role="button">5 Day Forecast</a></p>`;
   singleString += `</div>`;
   printToDom('#jumboWeather', singleString);
 };
@@ -18,16 +18,16 @@ const domString = (weatherArray) => {
   weatherArray.list.forEach((weather, i) => {
     if (i % 8 === 0) {
       weatherString += `<div class="col-sm-6 col-md-4 text-center weather">`;
-      weatherString += `<div class="thumbnail">`;
-      weatherString += `<div class="caption">`;
-      weatherString += `<img src="https://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="image of weather">`;
-      weatherString += `<h2 class="temperature">Temperature: ${weather.main.temp}</h2>`;
-      weatherString += `<h2 class="description">Description: ${weather.weather[0].description}</h2>`;
-      weatherString += `<h3 class="pressure"> Air Pressure: ${weather.main.pressure}</h3>`;
-      weatherString += `<h3 class="wind">Wind Speed: ${weather.wind.speed}</h3>`;
-      weatherString += `<p><a class="btn btn-primary btn-lg currentdayBtns" role="button">Current</a><a class="btn btn-warning btn-lg addWeatherToSaved" role="button">Save</a></p>`;
-      weatherString += `</div>`;
-      weatherString += `</div>`;
+      weatherString +=  `<div class="thumbnail">`;
+      weatherString +=    `<div class="caption">`;
+      weatherString +=      `<img src="https://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="image of weather">`;
+      weatherString +=        `<h2 class="temperature">Temperature: ${weather.main.temp}</h2>`;
+      weatherString +=         `<h2 class="description">Description: ${weather.weather[0].description}</h2>`;
+      weatherString +=          `<h3 class="pressure"> Air Pressure: ${weather.main.pressure}</h3>`;
+      weatherString +=          `<h3 class="wind">Wind Speed: ${weather.wind.speed}</h3>`;
+      weatherString +=          `<p><a class="btn btn-primary btn-lg currentdayBtns" role="button">Current</a><a class="btn btn-warning btn-lg addWeatherToSaved" role="button">Save</a></p>`;
+      weatherString +=   `</div>`;
+      weatherString +=  `</div>`;
       weatherString += `</div>`;
     };
   });
@@ -38,17 +38,24 @@ const savedDomString = (weatherArray, myCollectionMode = false) => {
   let weatherString = '';
   weatherArray.forEach((weather) => {
     weatherString += `<div class="col-sm-6 col-md-4 text-center weather">`;
-    weatherString += `<div class="thumbnail">`;
-    weatherString += `<div class="caption">`;
-    weatherString += `<h2 class="temperature"> ${weather.temperature}</h2>`;
-    weatherString += `<h2 class="description">${weather.condition}</h2>`;
-    weatherString += `<h3 class="pressure">${weather.pressure}</h3>`;
-    weatherString += `<h3 class="wind">Wind Speed: ${weather.wind}</h3>`;
-    if (!myCollectionMode) { // says if myColletion is true, opposite of argument above
-      weatherString += `<p><a class="btn btn-warning" role="button">?</a> <a class="btn btn-danger" role="button">Delete</a></p>`;
+    weatherString += `<div class="thumbnail data-firebase-id="${weather.id}">`;
+    weatherString +=    `<div class="caption">`;
+    if (myCollectionMode) {
+      weatherString +=    `<p><a class="btn btn-danger deleteWeatherFromCollectionEvent" role="button">X</a></p>`;
     }
-    weatherString += `</div>`;
-    weatherString += `</div>`;
+    weatherString +=      `<h2 class="temperature"> ${weather.temperature}</h2>`;
+    weatherString +=      `<h2 class="description">${weather.condition}</h2>`;
+    weatherString +=      `<h3 class="pressure">${weather.pressure}</h3>`;
+    weatherString +=      `<h3 class="wind">Wind Speed: ${weather.wind}</h3>`;
+    if (!myCollectionMode) { // says if myColletion is true, opposite of argument above
+      weatherString +=    `<p><a class="btn btn-warning" role="button">?</a></p>`;
+    } else if (myCollectionMode && !weather.isScarry) {
+      weatherString +=    `<p><a class="btn btn-primary updateMovieToWatched" role="button">I've Watched It</a></p>`;
+    } else {
+      weatherString +=    `<p>Not sure yet</p>`;
+    }
+    weatherString +=      `</div>`;
+    weatherString +=    `</div>`;
     weatherString += `</div>`;
   });
   printToDom('#savedWeather', weatherString);
